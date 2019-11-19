d2c4e78075b8ae57d167270a8a8d815d60fc6be7
unknown <rifdatulmufidah26041999@gmail.com>
2019-11-19 16:33:25 +0700

    mui

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Makna Kemerdekaan',
+      title: 'Kemerdekaan',
 //      theme: ThemeData(
 //        primarySwatch: Colors.red[600],
 //      ),
